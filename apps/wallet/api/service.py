4a128cc7419167ee0ff1c34e6b3dc37c33050700
hdforoozan <hdforoozan@gmail.com>
2022-05-12 16:08:20 +0430

    add transaction service

--- apps/wallet/api/service.py
+++ apps/wallet/api/service.py
@@ -7,3 +7,8 @@ class WalletService(WalletRepository):
         link = "https://api.blockchair.com/{}/dashboards/address/{}".format(request.data['blockchain_type'],
                                                                             request.data['address'])
         return self.addressInfo(link)
+
+    def getTransactionInfo(self, request):
+        link = "https://api.blockchair.com/{}/dashboards/transaction/{}".format(request.data['blockchain_type'],
+                                                                            request.data['transaction_hash'])
+        return self.transactionInfo(link)
