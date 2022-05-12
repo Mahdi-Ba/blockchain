82db29e54b07df6aebee903c11634c7fd32b38ed
hdforoozan <hdforoozan@gmail.com>
2022-05-12 16:09:10 +0430

    add trnasaction info method in repo

--- apps/wallet/api/repository.py
+++ apps/wallet/api/repository.py
@@ -4,3 +4,6 @@ import requests
 class WalletRepository:
     def addressInfo(self, link):
         return requests.get(link).json()
+
+    def transactionInfo(self, link):
+        return requests.get(link).json()
