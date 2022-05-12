3389bcd4bb8da34c41d232681e884d6ed5f3b1f4
hdforoozan <hdforoozan@gmail.com>
2022-05-12 16:07:08 +0430

    add transaction url

--- apps/wallet/urls.py
+++ apps/wallet/urls.py
@@ -4,4 +4,5 @@ from .api import views
 
 urlpatterns = [
     path('address/info', views.AddressInfo.as_view(), name=None),
+    path('transaction/info', views.TransactionInfo.as_view(), name=None),
 ]
